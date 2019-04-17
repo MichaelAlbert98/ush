5ff515234438631ae88401ca4b1107a78000946d
MichaelAlbert98 <albertm4@wwu.edu>
2019-04-16 19:41:48 -0700

    Intial structure.

---
+++ builtin.c
@@ -0,0 +1,25 @@
+/*   CS 347 -- Builtin
+ *
+ *   April 16, 2019, Michael Albert
+ *   Modified April 17, 2019
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "defn.h"
+
+/* Prototypes */
+
+//none yet!
+
+/* Builtin */
+
+int isbuiltin (char **parsedargs, int argc) {
+  return 0;
+}
+
+void execbuiltin (char **parsedargs, int argc) {
+  return;
+}
