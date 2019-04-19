53ae9d371e3455bf8a232f54cd1042e2e5bf9ab1
MichaelAlbert98 <albertm4@wwu.edu>
2019-04-18 18:04:27 -0700

    Fully implemented.

--- builtin.c
+++ builtin.c
@@ -8,18 +8,89 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 #include "defn.h"
+
+/* Constants */
+
+#define BUILTINNUM 4
+const char* builtins[] = { "exit", "envset", "envunset", "cd"};
 
 /* Prototypes */
 
-//none yet!
+void exitbuilt (char **parsedargs, int argc);
+void envsetbuilt (char **parsedargs, int argc);
+void envunsetbuilt (char **parsedargs, int argc);
+void cdbuilt (char **parsedargs, int argc);
 
 /* Builtin */
 
 int isbuiltin (char **parsedargs, int argc) {
+  /* Check if command is a builtin */
+  for (int i = 0; i < BUILTINNUM; i++) {
+    if (strcmp(parsedargs[0], builtins[i]) == 0) {
+      /* Call corresponding builtin helper */
+      void (*funcptrarr[])(char**, int) = {exitbuilt, envsetbuilt, envunsetbuilt, cdbuilt};
+      (*funcptrarr[i])(parsedargs, argc);
+      return 1;
+    }
+  }
+  /* Not a builtin */
   return 0;
 }
 
-void execbuiltin (char **parsedargs, int argc) {
+void exitbuilt (char **parsedargs, int argc) {
+  /* Print error */
+  if (argc > 2) {
+    fprintf(stderr, "Incorrect number of arguments.\n");
+  }
+  /* Exit with given value */
+  else if (argc == 2) {
+    exit(atoi(parsedargs[1]));
+  }
+  /* Default exit */
+  else {
+    exit(0);
+  }
+  return;
+}
+
+void envsetbuilt (char **parsedargs, int argc) {
+  /* Print error */
+  if (argc != 3) {
+    fprintf(stderr, "Incorrect number of arguments.\n");
+  }
+  /* Set the given environment. No overwriting */
+  else {
+    setenv(parsedargs[1],parsedargs[2],0);
+  }
+  return;
+}
+
+void envunsetbuilt (char **parsedargs, int argc) {
+  /* Print error */
+  if (argc != 2) {
+    fprintf(stderr, "Incorrect number of arguments.\n");
+  }
+  /* Remove environment with given name */
+  else {
+    unsetenv(parsedargs[1]);
+  }
+  return;
+}
+
+void cdbuilt (char **parsedargs, int argc) {
+  /* Print error */
+  if (argc > 2) {
+    fprintf(stderr, "Incorrect number of arguments.\n");
+  }
+  /* Change directory to given path */
+  else if (argc == 2){
+    chdir(parsedargs[1]);
+  }
+  /* Default go home */
+  else {
+    chdir(getenv("HOME"));
+  }
   return;
 }
