aa7b0dbaaeb69a44af05d43d88ca73a6f2b87045
albertmichael98 <albertm4@wwu.edu>
2019-05-05 19:44:46 -0700

    Global argc and argv.

---
+++ globals.h
@@ -0,0 +1,12 @@
+/*   CS 347 -- Global Vars
+ *
+ *   May 05, 2019, Michael Albert
+ *   Modified May 05, 2019
+ *
+ */
+
+ #pragma once
+
+/* Global Vars */
+extern int globalargc;  
+extern char **globalargv;  
